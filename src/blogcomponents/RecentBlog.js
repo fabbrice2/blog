b8ce50504c8fb892e09b49cdada1b03fe5e2a044
fabbrice2 <fabriceamenouglo@gmail.com>
2025-02-25 10:22:57 +0100

    UPDATE

--- src/blogcomponents/RecentBlog.js
+++ src/blogcomponents/RecentBlog.js
@@ -1,9 +1,9 @@
 import React from "react";
 import CustomButton from "../ui/CustomButton";
-import Image from "../assets/Image.png"
-import Image2 from "../assets/Image2.png"
-import Image3 from "../assets/Image3.png"
-import Image4 from "../assets/Image4.png"
+import Image from "../assets/Image.png";
+import Image2 from "../assets/Image2.png";
+import Image3 from "../assets/Image3.png";
+import Image4 from "../assets/Image4.png";
 import TextComponent from "../functions/TextComponent";
 
 function RecentBlog() {
@@ -13,11 +13,7 @@ function RecentBlog() {
       <div className="flex flex-col gap-5">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
           <div className="flex flex-col gap-5">
-            <img
-              className="h-60 w-full object-cover"
-              src={Image}
-              alt=""
-            />
+            <img className="h-60 w-full object-cover" src={Image} alt="" />
             <div className=" space-y-2">
               <span className="text-purple font-bold">Sunday , 1 Jan 2023</span>
               <a href="#" className="flex justify-between text-lg font-bold">
@@ -48,12 +44,8 @@ function RecentBlog() {
             </div>
           </div>
           <div className="flex flex-col gap-8 sm:gap-0 sm:justify-between">
-            <div className="grid grid-cols-1 md:grid-cols-2 items-center sm:space-x-5">
-              <img
-                className="h-full object-cover"
-                src={Image2}
-                alt=""
-              />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 items-center">
+              <img className="h-60 w-full object-cover" src={Image2} alt="" />
               <div className="space-y-2">
                 <span className="text-purple font-bold">
                   Sunday , 1 Jan 2023
@@ -80,12 +72,8 @@ function RecentBlog() {
                 </div>
               </div>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 items-center sm:space-x-5">
-              <img
-                className="h-full object-cover"
-                src={Image3}
-                alt=""
-              />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 items-center py-6 ">
+              <img className="h-60 w-full object-cover" src={Image3} alt="" />
               <div className="space-y-2">
                 <span className="text-purple font-bold">
                   Sunday , 1 Jan 2023
@@ -115,11 +103,7 @@ function RecentBlog() {
           </div>
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 my-8">
-          <img
-            className="h-60 w-full object-cover"
-            src={Image4}
-            alt=""
-          />
+          <img className="h-60 w-full object-cover" src={Image4} alt="" />
           <div className="flex flex-col space-y-2 sm:space-y-0 sm:justify-between">
             <span className="text-purple font-bold">Sunday , 1 Jan 2023</span>
             <a href="#" className="flex justify-between text-lg font-bold">
